dd4e63a41bec15ef504b2b537fb8b6c5e15e5385
jgillespie <jgillespie17@wou.edu>
2019-10-18 18:03:39 -0700

    added potential queueinterface.cs port from java

---
+++ HW3/HW3/HW3/QueueInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW3
+{
+
+
+    public interface QueueInterface<T>
+    {
+        T push(T element);
+
+        T pop();
+
+        T peek();
+
+        bool isEmpty();
+    }
+}
