e4e4052c606fec5c28433a37fe662db0bd50a2e6
jgillespie <jgillespie17@wou.edu>
2019-10-18 22:05:42 -0700

    added implementation of Linked Queue and Queue underflow Exception into C#

---
+++ HW3/HW3/HW3/LinkedQueue.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace HW3
+{
+    public class LinkedQueue<T> : QueueInterface<T>
+    {
+        private Node<T> front;
+        private Node<T> rear;
+
+        public LinkedQueue()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public T push(T element)
+        {
+            if(element == null)
+            {
+                throw new nullPointerException();    
+            }
+
+            if (isEmpty())
+            {
+                Node<T> tmp = new Node<T>(element, null);
+                rear = front = tmp;
+            }
+            else
+            {
+                Node<T> tmp = new Node<T>(element, null);
+                rear.next = tmp;
+                rear = tmp;
+            }
+            return element;
+        }
+        
+        public T pop()
+        {
+            T tmp = null;
+            if (isEmpty())
+            {
+                throw new QueueUnderflowException("The queue was empty when pop was invoked.");
+            }
+            else if (front == rear)
+            {
+                tmp = front.data;
+                front = null;
+                rear = null;
+            }
+            else
+            {
+                tmp = front.data;
+                front = front.next;
+            }
+            return tmp;
+               
+        }
+
+        public T peek()
+        {
+            if (isEmpty())
+            {
+                throw new QueueUnderflowException("The queue was empty when peek was invoked.");
+            }
+            return front.data;
+        }
+
+        public bool isEmpty()
+        {
+            if(front == null && rear == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+            
+
+    }
+
+}
---
+++ HW3/HW3/HW3/QueueUnderflowException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW3
+{
+    public class QueueUnderflowException
+    {
+        public QueueUnderflowException()
+        {
+            base.processorOperator();
+        }
+         public QueueUnderflowException(String message)
+        {
+            base.processorOperator(message);
+        }
+            
+
+    }
+
+}
+
