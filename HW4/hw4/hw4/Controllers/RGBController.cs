7c3dda060adbe1d7d0460680cd7e0d13e2b6e4c4
jgillespie <jgillespie17@wou.edu>
2019-10-26 17:24:23 -0700

    Finished cosmetic work on home page, began work on RGB controller

---
+++ HW4/hw4/hw4/Controllers/RGBController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hw4.Controllers
+{
+    public class RGBController : Controller
+    {
+        // GET: RGB
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult index()
+        {
+
+        }
+    }
+}
