b88ce33128f059220fb6e68e669f258fd604561b
jgillespie <jgillespie17@wou.edu>
2019-10-26 23:58:26 -0700

    Added user input and checks for input

--- HW4/hw4/hw4/Controllers/RGBController.cs
+++ HW4/hw4/hw4/Controllers/RGBController.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
 
 namespace hw4.Controllers
 {
     public class RGBController : Controller
     {
+
         // GET: RGB
         public ActionResult Index()
         {
+            string RedS = Request.QueryString["intRed"];
+            int Red = Convert.ToInt32(RedS);
+            string GreenS = Request.QueryString["intGreen"];
+            int Green = Convert.ToInt32(GreenS);
+            string BlueS = Request.QueryString["intBlue"];
+            int Blue = Convert.ToInt32(BlueS);
+            
+           
+            Color NewColor = Color.FromArgb(1, Red, Green, Blue);
+            HexStruct output = new HexStruct
+            {
+                Hex = NewColor.R.ToString("X2") + NewColor.G.ToString("X2") + NewColor.B.ToString("X2")
+            };
+            ViewBag.Success = true;
+            ViewBag.HexColor = output;
+
+
+
+
             return View();
         }
-
-        [HttpPost]
-        public ActionResult index()
+        public struct HexStruct
         {
+            public string Hex { get; set; }
+
 
         }
+        
+
+       
     }
 }
