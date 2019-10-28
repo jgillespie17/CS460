7756ad7ab8bc2914e0f6ec0f164424f547eb3f22
jgillespie <jgillespie17@wou.edu>
2019-10-27 20:47:47 -0700

    Found possible solution to color interpolation and implemented code from assignment page, added colorinterpolation controller/view

--- HW4/hw4/hw4/Controllers/HomeController.cs
+++ HW4/hw4/hw4/Controllers/HomeController.cs
@@ -31,13 +31,5 @@ namespace hw4.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
     }
 }
