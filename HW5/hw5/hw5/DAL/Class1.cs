43eec5ba50544d1f65ea3791e519b68339e6b74c
jgillespie <jgillespie17@wou.edu>
2019-11-02 18:18:21 -0700

    Added model, DAL, and stylized tracker page using html helpers

---
+++ HW5/hw5/hw5/DAL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using hw5.Models;
+
+namespace hw5.DAL
+{
+    public class HomeworkContext : DbContext
+    {
+        public HomeworkContext() : base("name=Homework")
+        {
+
+        }
+
+        public virtual DbSet<Homework>Homework { get; set; }
+    }
+}
---
+++ HW5/hw5/hw5/Models/Homework.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hw5.Models
+{
+    public class Homework
+    {
+        public int Urgency { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime DueTime { get; set; }
+        public string Department { get; set; }
+        public int CourseNum { get; set; }
+        public string Title { get; set; }
+        public string Notes { get; set; }
+    }
+}
