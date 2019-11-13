97fec3d8eedca4d2bef67fd724e28c5fbe3f61bc
jgillespie <jgillespie17@wou.edu>
2019-11-12 21:06:24 -0800

    Added search bar and remove extra views from home controller

--- HW6/hw6/hw6/Controllers/HomeController.cs
+++ HW6/hw6/hw6/Controllers/HomeController.cs
@@ -13,17 +13,9 @@ namespace hw6.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(string name)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
--- HW6/hw6/hw6/Global.asax.cs
+++ HW6/hw6/hw6/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Types;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,11 @@ namespace hw6
     {
         protected void Application_Start()
         {
+
+            SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~/bin"));
+
+            SqlProviderServices.SqlServerTypesAssemblyName = typeof(SqlGeography).Assembly.FullName;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
