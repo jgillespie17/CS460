083d22734fe6ef752bc4d2c43eddea48706ce023
jgillespie <jgillespie17@wou.edu>
2019-11-14 13:51:59 -0800

    added styling to details page and photo placeholder if no phot avaliable

--- HW6/hw6/hw6/Models/ViewModel/StockItemDetailsViewModel.cs
+++ HW6/hw6/hw6/Models/ViewModel/StockItemDetailsViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Script.Serialization;
 
@@ -20,28 +22,38 @@ namespace hw6.Models.ViewModel
             //var json = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(stockItem.CustomFields);
             JObject json = JObject.Parse(stockItem.CustomFields);
             StockItemOrigin = (string)json["CountryOfManufacture"];
-            StockItemTags = json["Tags"].ToString();
+            //StockItemTags = ;
+            string pattern = @"^(\[){1}(.*?)(\]){1}$";
+            StockItemTags = Regex.Replace(json["Tags"].ToString(), pattern, "");
             StockItemPhoto = stockItem.Photo;
 
 
         }
-
+        [Display(Name = "Name: ")]
         public string StockItemName { get; set; }
 
+        [Display(Name = "Size: ")]
         public string StockItemSize { get; set; }
 
+        [Display(Name = "Price(in USD): ")]
         public decimal StockItemPrice { get; set; }
 
+        [Display(Name = "Weight: ")]
         public decimal StockItemWeight { get; set; }
 
+        [Display(Name = "Lead Time(in days): ")]
         public int StockItemLeadTime { get; set; }
 
+        [Display(Name = "Valid from: ")]
         public DateTime StockItemDate { get; set; }
 
+        [Display(Name = "Origin: ")]
         public string StockItemOrigin { get; set; }
 
+        [Display(Name = "Tags: ")]
         public string StockItemTags { get; set; }
 
+        [Display(Name = "Photo: ")]
         public byte[] StockItemPhoto { get; set; }
     }
 }
