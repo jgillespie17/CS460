d09235bcb2a1c642a1b9feabf33345fce5ec6c9f
jgillespie <jgillespie17@wou.edu>
2019-11-13 19:13:49 -0800

    Continuing work on view for feature one

--- HW6/hw6/hw6/Controllers/HomeController.cs
+++ HW6/hw6/hw6/Controllers/HomeController.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using hw6.DAL;
+using hw6.Models;
 
 namespace hw6.Controllers
 {
     public class HomeController : Controller
     {
+
+        private WWIContext db = new WWIContext();
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
         public ActionResult Index(string name)
         {
-            return View();
+            //IList<StockItem> items = new List<StockItem>();
+            var matchingitems = db.StockItems.Where(s => s.Contains(name));
+            StockItem item = db.StockItems.Find(name);           
+            if(item == null)
+            {
+                return HttpNotFound();
+
+            }
+            return View(item);
         }
     }
 }
