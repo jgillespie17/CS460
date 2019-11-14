f6c86f49924d3371ac4ca687ebeb954421775977
jgillespie <jgillespie17@wou.edu>
2019-11-13 22:40:07 -0800

    Started work on details page with a viewmodel

---
+++ HW6/hw6/hw6/Controllers/StockItemController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using hw6.DAL;
+using hw6.Models;
+
+namespace hw6.Controllers
+{
+    public class StockItemController : Controller
+    {
+        private WWIContext db = new WWIContext();
+        // GET: StockItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockItem stockItem = db.StockItems.Find(id);
+            if(stockItem == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(stockItem);
+        }
+    }
+}
---
+++ HW6/hw6/hw6/Models/ViewModel/StockItemDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hw6.Models.ViewModel
+{
+    public class StockItemDetailsViewModel
+    {
+        public StockItemDetailsViewModel(StockItem stockItem)
+        {
+
+        }
+
+        public string StockItemName { get; set; }
+
+        public string StockItemSize { get; set; }
+    }
+}
