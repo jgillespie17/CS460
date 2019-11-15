1a1e5b70f81f55cd873aea991f91c59cab3db769
jgillespie <jgillespie17@wou.edu>
2019-11-14 17:08:39 -0800

    Finalized homework and met both feature requirements

--- HW6/hw6/hw6/Models/ViewModel/StockItemDetailsViewModel.cs
+++ HW6/hw6/hw6/Models/ViewModel/StockItemDetailsViewModel.cs
@@ -13,38 +13,53 @@ namespace hw6.Models.ViewModel
     {
         public StockItemDetailsViewModel(StockItem stockItem)
         {
+            //Stock Item details 
             StockItemName = stockItem.StockItemName;
             StockItemSize = stockItem.Size;
-            StockItemPrice = stockItem.UnitPrice;
+            StockItemPrice = stockItem.RecommendedRetailPrice;
             StockItemWeight = stockItem.TypicalWeightPerUnit;
             StockItemLeadTime = stockItem.LeadTimeDays;
             StockItemDate = stockItem.ValidFrom;
-            //var json = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(stockItem.CustomFields);
             JObject json = JObject.Parse(stockItem.CustomFields);
             StockItemOrigin = (string)json["CountryOfManufacture"];
-            //StockItemTags = ;
-            string pattern = @"^(\[){1}(.*?)(\]){1}$";
-            StockItemTags = Regex.Replace(json["Tags"].ToString(), pattern, "");
+            StockItemTags = stockItem.Tags.Trim('[', ']');
             StockItemPhoto = stockItem.Photo;
 
+            //Stock item supplier details
+            SupplierName = stockItem.Supplier.SupplierName;
+            SupplierPhone = stockItem.Supplier.PhoneNumber;
+            SupplierFax = stockItem.Supplier.FaxNumber;
+            SupplierWebsite = stockItem.Supplier.WebsiteURL;
+            SupplierContact = stockItem.Supplier.Person2.FullName;
+
+            //Sales details
+            SalesOrderNum = stockItem.OrderLines.Count();
+            SalesGrossSales = stockItem.InvoiceLines.Sum(x => x.ExtendedPrice);
+            SalesGrossProfit = stockItem.InvoiceLines.Sum(x => x.LineProfit);
+
+            //Disgusting Linq
+            Top10List = stockItem.InvoiceLines.GroupBy(y => y.Invoice.Customer.CustomerName).Select(x => (CustomerName: x.Key, Orders: x.Sum(z => z.Quantity))).OrderByDescending(x => x.Orders).Take(10);
 
         }
+        //StockItem details
         [Display(Name = "Name: ")]
         public string StockItemName { get; set; }
 
         [Display(Name = "Size: ")]
         public string StockItemSize { get; set; }
 
-        [Display(Name = "Price(in USD): ")]
-        public decimal StockItemPrice { get; set; }
+        [Display(Name = "Price: ")]
+        [DataType(DataType.Currency)]
+        public decimal? StockItemPrice { get; set; }
 
         [Display(Name = "Weight: ")]
         public decimal StockItemWeight { get; set; }
 
-        [Display(Name = "Lead Time(in days): ")]
+        [Display(Name = "Lead Time: ")]
         public int StockItemLeadTime { get; set; }
 
         [Display(Name = "Valid from: ")]
+        [DataType(DataType.Date)]
         public DateTime StockItemDate { get; set; }
 
         [Display(Name = "Origin: ")]
@@ -55,5 +70,35 @@ namespace hw6.Models.ViewModel
 
         [Display(Name = "Photo: ")]
         public byte[] StockItemPhoto { get; set; }
+
+
+        //Stock item supplier profile
+        [Display(Name = "Company")]
+        public string SupplierName { get; set; }
+
+        [Display(Name = "Phone")]
+        public string SupplierPhone { get; set; }
+
+        [Display(Name = "Fax")]
+        public string SupplierFax { get; set; }
+
+        [Display(Name = "Website")]
+        public string SupplierWebsite { get; set; }
+
+        [Display(Name = "Contact")]
+        public string SupplierContact { get; set; }
+
+        //Sales History Summary
+        [Display(Name = "Orders: ")]
+        public int SalesOrderNum { get; set; }
+        [Display(Name = "Gross Sales: ")]
+        [DataType(DataType.Currency)]
+        public decimal SalesGrossSales { get; set; }
+        [Display(Name = "Gross Profit: ")]
+        [DataType(DataType.Currency)]
+        public decimal SalesGrossProfit { get; set; }
+
+        //Gross list of gross stuff
+        public IEnumerable<(string name, int quantity)> Top10List { get; set; }
     }
 }
